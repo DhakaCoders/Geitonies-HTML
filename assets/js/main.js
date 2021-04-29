60792e49085b9ec245ec2453a01f3fd1fda2f934
Rannojit <cserannojitsarkar22@gmail.com>
2021-04-29 16:11:16 +0600

    .

--- assets/js/main.js
+++ assets/js/main.js
@@ -226,13 +226,20 @@ if( $('.fxParkSlider').length ){
 
 
 
-/*start of Rannojit*/
+
 
 
 
 /*start of Shariful*/
 
 
+
+
+/*start of Rannojit*/
+
+$('.map-sidebar-btn').on('click', function(){
+  $('.map-sidebar-cntlr').toggleClass('map-sidebar-expend');
+});
 
 /*
 mapboxgl.accessToken = 'pk.eyJ1IjoicmFuaml0c2Fya2FyIiwiYSI6ImNrbzEzMHcxczA2bzgyb2xiZ3E0ZTk5djcifQ.ZmboMHcA3P9QZnmxZLcWaw';
